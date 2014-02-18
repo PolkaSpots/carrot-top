009da753cfacad59103faeea076461898bde84dd
Simon M <simon@polkaspots.com>
2014-02-18 23:54:03 +0000

    put some test stuff in

--- lib/carrot-top.rb
+++ lib/carrot-top.rb
@@ -41,11 +41,12 @@ class CarrotTop
     raise ArgumentError, "HTTP redirect too deep" if limit == 0
     url = URI.parse(uri)
     # REMOVED FOR TESTING #
-    #if @proxy_host
+    # THIS IS JUST A TEST #
+    if Rails.env.test? or Rails.end.development?
+      http = Net::HTTP.new(url.host, url.port)
+    else
       http = Net::HTTP::Proxy(@proxy_host, @proxy_port).new(url.host, url.port)
-    #else
-    #  http = Net::HTTP.new(url.host, url.port)
-    #end
+    end
     if url.scheme == "https"
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
